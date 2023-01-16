c4dcc216b536352294a104214b331437c64230c4
Sina Haghighi <34384561+Sinahag@users.noreply.github.com>
2023-01-16 15:52:48 -0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "0GcTBsB7OMjwUUvrr6XWU5us3NLYon5j"
-    CSRF_ENABLED     = True
-    CSRF_SESSION_KEY = "secret"
-    MONGODB_SETTINGS = { 'db' : 'portfolio' , 
-    'host' : 'mongodb+srv://sinahaghighi:Sinaliana2001@portfolio.iqjbhut.mongodb.net/?retryWrites=true&w=majority' } 
